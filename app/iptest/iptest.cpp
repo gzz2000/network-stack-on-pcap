8da4f12e07621ad7747c1ec84566a34f8c0b24bd
GZZ <gzz_2000@126.com>
2020-11-12 19:16:47 -0800

    Test demo added for sending IP packets

--- app/iptest/iptest.cpp
+++ app/iptest/iptest.cpp
@@ -1,4 +1,6 @@
 #include <cstdio>
+#include <cstring>
+#include <arpa/inet.h>
 #include "ip/ip.hpp"
 
 int ipPacketCallback(const void *buf, int len) {
@@ -13,7 +15,23 @@ int main() {
     startIPService(scanAllDevices("veth"));
     //Enter REPL
     while(true) {
-        getchar();   // do nothing this time
+        // input like: <src IP> <dest IP> content
+        // eg: 10.100.1.1 10.100.3.1 helloworld
+        char srcip[20], destip[20], content[100];
+        scanf("%s%s%s", srcip, destip, content);
+        struct in_addr addr_src, addr_dest;
+        if(inet_aton(srcip, &addr_src) == 0) {
+            fprintf(stderr, "[App Error] IP parse failed: %s\n", srcip);
+            continue;
+        }
+        if(inet_aton(destip, &addr_dest) == 0) {
+            fprintf(stderr, "[App Error] IP parse failed: %s\n", destip);
+            continue;
+        }
+        int ret = sendIPPacket(addr_src.s_addr, addr_dest.s_addr,
+                               253 /* IANA reserved test protocol */,
+                               content, strlen(content));
+        fprintf(stderr, "sendIPPacket returns %d\n", ret);
     }
     return 0;
 }
